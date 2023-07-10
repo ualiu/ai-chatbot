a7f6c02d8eae6ce8767fed84d8bcb218799b3cb3
ualiu <urim.aliu@gmail.com>
2023-07-10 14:29:50 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,56 @@
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
+
+const openai = new OpenAIApi(configuration)
+
+const chatbotConversation = document.getElementById('chatbot-conversation')
+ 
+let conversationStr = ''
+ 
+document.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const userInput = document.getElementById('user-input') 
+    conversationStr += ` ${userInput.value} ->`
+    console.log(conversationStr)
+    fetchReply()
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-human')
+    chatbotConversation.appendChild(newSpeechBubble)
+    newSpeechBubble.textContent = userInput.value
+    userInput.value = ''
+    chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+}) 
+
+async function fetchReply(){
+    const response = await openai.createCompletion({
+        model: 'davinci:ft-personal-2023-07-10-04-19-35',
+        prompt: conversationStr,
+        presence_penalty: 0,
+        frequency_penalty: 0.3,
+        max_tokens: 100,
+        temperature: 0,
+        stop: ['\n', '->'],
+    })
+    conversationStr += ` ${response.data.choices[0].text} \n`
+    renderTypewriterText(response.data.choices[0].text)
+    console.log(conversationStr)
+}
+
+function renderTypewriterText(text) {
+    const newSpeechBubble = document.createElement('div')
+    newSpeechBubble.classList.add('speech', 'speech-ai', 'blinking-cursor')
+    chatbotConversation.appendChild(newSpeechBubble)
+    let i = 0
+    const interval = setInterval(() => {
+        newSpeechBubble.textContent += text.slice(i-1, i)
+        if (text.length === i) {
+            clearInterval(interval)
+            newSpeechBubble.classList.remove('blinking-cursor')
+        }
+        i++
+        chatbotConversation.scrollTop = chatbotConversation.scrollHeight
+    }, 50)
+}
